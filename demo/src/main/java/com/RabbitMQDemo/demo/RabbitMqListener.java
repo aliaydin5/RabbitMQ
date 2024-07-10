302363620b5d5260944aeccf84c0ce909a51d919
aliaydin34 <120198127+aliaydin34@users.noreply.github.com>
2024-07-10 13:09:32 +0300

    güncelleme

--- demo/src/main/java/com/RabbitMQDemo/demo/RabbitMqListener.java
+++ demo/src/main/java/com/RabbitMQDemo/demo/RabbitMqListener.java
@@ -4,6 +4,11 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 @Service
 @EnableRabbit
 public class RabbitMqListener {
@@ -13,5 +18,16 @@ public class RabbitMqListener {
 
 
     }
+
+    @RabbitListener(queues = "Mobile")
+    public void readMessage(byte[] message) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bis=new ByteArrayInputStream(message);
+        ObjectInput in=new ObjectInputStream(bis);
+        User user=(User) in.readObject();
+        in.close();
+        bis.close();
+        System.out.println(user.getName());
+    }
+
 
 }
